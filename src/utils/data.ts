df8bc2ed0d123db3090eba6a42bacffb046f94c5
thmscode <102998926+thmscode@users.noreply.github.com>
2024-04-17 21:22:06 -0400

    Create data.ts
    Mock data for the bookstore

---
+++ src/utils/data.ts
@@ -0,0 +1,42 @@
+export const BOOKS = [
+  {
+    id: "8pi0Ilv8fueQD-7yVva2h",
+    title: "neverwhere",
+    price: "24.99",
+    category: "urban fantasy",
+    description:
+      "Under the streets of London there's a world most people could never even dream of. A city of monsters and saints, murderers and angels, knights in armour and pale girls in black velvet. Strange destinies lie in wait in London below - a world that seems eerily familiar.",
+  },
+  {
+    id: "HU_UkDm4cDbNEIx3MFO33",
+    title: "a man called ove",
+    price: "19.99",
+    category: "humourous fiction",
+    description:
+      "Meet Ove. He's a curmudgeon, the kind of man who points at people he dislikes as if they were burglars caught outside his bedroom window. He has staunch principles, strict routines, and a short fuse. People call him the bitter neighbor from hell, but must Ove be bitter just because he doesn't walk around with a smile plastered to his face all the time?",
+  },
+  {
+    id: "VtLBdTNa9cqCrkJApr6rj",
+    title: "the hitchhiker's guide to the galaxy",
+    price: "17.99",
+    category: "science fiction",
+    description:
+      "Seconds before the Earth is demolished to make way for a galactic freeway, Arthur Dent is plucked off the planet by his friend Ford Prefect, a researcher for the revised edition of The Hitchhiker's Guide to the Galaxy.",
+  },
+  {
+    id: "9qwOk4NeIeP9aunN103Gf",
+    title: "american gods",
+    price: "33.50",
+    category: "fantasy",
+    description:
+      "Days before his release from prison, Shadow's wife, Laura, dies in a mysterious car crash. Numbly, he makes his way back home. On the plane, he encounters the enigmatic Mr Wednesday, who claims to be a refugee from a distant war, a former god and the king of America.",
+  },
+  {
+    id: "eF5sifMoYv8O3javni9b5",
+    title: "harry potter and the prisoner of azkaban",
+    price: "14.00",
+    category: "fantasy",
+    description:
+      "Harry Potter, along with his best friends, Ron and Hermione, is about to start his third year at Hogwarts School of Witchcraft and Wizardry. Harry can't wait to get back to school after the summer holidays. But when Harry gets to Hogwarts, the atmosphere is tense.",
+  },
+];
